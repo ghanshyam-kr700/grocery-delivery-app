b3804a412aa7f708d2789f16ef8eae896b4c69f3
7004610882 <ghanshyam7004610@gmail.com>
2026-05-31 23:42:09 +0530

    Updated product page, order tracking UI and bug fixes

--- src/pages/OrderTracking.tsx
+++ src/pages/OrderTracking.tsx
@@ -1,9 +1,88 @@
-
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dummyDashboardOrdersData } from "../assets/assets";
+import Loading from "../Components/Loading";
+import { ArrowLeftIcon } from "lucide-react";
+import OrderOTP from "../Components/OrderRecords/OrderOTP";
+// import { dummyDashboardData } from "../assets/assets";
+// import type { Order } from "../types";
 
 const OrderTracking = () => {
-  return (
-    <div>OrderTracking</div>
-  )
-}
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default OrderTracking
+  const [order, setOrder] = useState<any | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const [liveLocation, setLiveLocation] = useState<{
+    lat: number;
+    lng: number;
+  } | null>(null);
+
+  useEffect(() => {
+    setOrder(dummyDashboardOrdersData.find((o) => o._id === id) as any);
+
+    setLoading(false);
+  }, [id]);
+
+  if (loading) return <Loading />;
+  if (!order) return null;
+
+  return (
+    <div className="min-h-screen mb-20 bg-app-cream">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        {/* Header */}
+        <button
+          onClick={() => navigate("/orders")}
+          className="flex items-center gap-2 text-sm text-app-text-light hover:text-app-green mb-6 transition-colors"
+        >
+          <ArrowLeftIcon className="size-4" />
+          Back to orders
+        </button>
+
+        {/* order id,date, status */}
+        <div className="flex items-center justify-between mb-8">
+          <div>
+            <h1 className="text-2xl font-semibold text-app-green">
+              Order #{order._id.slice(-8).toUpperCase()}
+            </h1>
+
+            <p className="text-sm text-app-text-light mt-1">
+              Placed on{" "}
+              {new Date(order.createdAt).toLocaleDateString("en-US", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </p>
+          </div>
+
+          <span
+            className={`px-4 text-sm font-semibold rounded-full ${order.status === "Delivered"
+              ? "bg-green-100 text-green-700"
+              : order.status === "cancelled"
+                ? "bg-red-100 text-red-700"
+                : "bg-app-orange/10 text-app-orange"
+              }`}
+          >
+            {order.status}
+          </span>
+        </div>
+
+        <div className="grid lg:grid-cols-3 gap-6">
+          {/*Lefft site  Timeline + map Area  */}
+          <div className="lg:col-span-2 space-y-6">
+            {/* OTP Card  */}
+            <OrderOTP order={order} />
+
+
+          </div>
+          {/*Right side - order Details  */}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderTracking;
